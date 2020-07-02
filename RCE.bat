8cff70612918b98058e2b9a3c1b8ebbecefbd2eb
z0mbieslayer <44008070+z0mbieslayer@users.noreply.github.com>
2020-07-02 18:05:21 -0400

    Improved URL generator

--- RCE.bat
+++ RCE.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBdbSwWMAE+1EbsQ5+n//Na1rU8RW/p/W47P27iBJ6A28lXrZ4IoxEZol8kAAwMYXRO/bwM7ri5nrnSKJdOZoUHkUk3p
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 title Roblox Catalog Exporter by z0mbieslayer
 echo This program will export useful json info about a roblox catalog item and arrange it into an html table.
@@ -46,11 +79,14 @@ curl -s https://api.roblox.com/Marketplace/ProductInfo?assetId=%catalogid% -o it
 echo ^| Converting raw json to html...                                                                                      ^|
 setlocal enabledelayedexpansion
 FOR /F "delims=" %%i IN ('jq .Name item.json') DO set itemname=%%i
+SET invalid=-
 set NEW_TEXT=%itemname: =!invalid!%
+SET invalid2=(
 set NEW_TEXT2=%NEW_TEXT:(=!invalid2!%
+SET invalid3=)
 set NEW_TEXT3=%NEW_TEXT2:)=!invalid3!%
-set NEW_TEXT4=%NEW_TEXT3:?=!invalid4!%
-set itemnamefixed=%NEW_TEXT4:"=!invalid5!%
+SET invalid4=!
+set itemnamefixed=%NEW_TEXT3:?=!invalid4!%
 setlocal enabledelayedexpansion
 set "SEARCHTEXT="PriceInTickets":null,"
 set "REPLACETEXT="ItemURL":"https://roblox.com/catalog/catalogid/itemname","
